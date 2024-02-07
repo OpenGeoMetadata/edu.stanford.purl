543f70d34cdd8380ce152b34626cbba43ca24b8d
Nick Budak <budak@stanford.edu>
2024-02-07 12:58:36 -0800

    Remove unused script

--- doit.rb
+++
@@ -1,8 +0,0 @@
-require 'json'
-# ./bb/338/jh/0716/geoblacklight.json
-data = {}
-$stdin.readlines.each do |line|
-  druid = File.dirname(line).split(/\//).join
-  data["druid:#{druid}"] = File.dirname(line)
-end
-puts JSON.pretty_generate(data)
